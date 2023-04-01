5d2b7389896f00c2b33729d73429bd17a364336e
Zafar <mzafary.iqbal@yahoo.com>
2023-04-02 04:34:31 +0500

    Added python file

---
+++ test.py
@@ -0,0 +1,18 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+
+elen = turtle.Turtle()
+elen.color("brown")
+elen.width(12)
+
+for side in [1,2,3,4, 5, 6]:
+    elen.penup()
+    elen.forward(20)
+    elen.pendown()
+    for _ in range(6):
+        elen.right(60)
+        elen.forward(60)
+    elen.right(300)
+
+wn.exitonclick()
